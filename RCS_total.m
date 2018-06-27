220d0887b8e3661382f81b4bbf6998a59bd1469e
mlbombardier <40322391+mlbombardier@users.noreply.github.com>
2018-06-27 09:07:05 -0700

    Add files via upload

---
+++ RCS_total.m
@@ -0,0 +1,41 @@
+% plot and spectrogram of filtered RCS data from IRIS
+
+%close all
+
+ct = ChannelTag('UW','RCS','*','EHZ');
+tr = irisFetch.Traces(ct.network,ct.station,ct.location,ct.channel,'2014-11-25 20:00:00','2014-11-25 23:59:59');
+
+data = tr.data;
+fSamp = tr.sampleRate;
+fNy = fSamp/2.0;
+fhi = 20;
+
+window = ceil(50.0*fNy);
+step = ceil(10.0*fNy);
+t = linspace(0.0, size(data,1)/fSamp, size(data,1));
+
+% create filter and apply it to the data
+k = fir1(256, fhi/fNy, 'low');
+dfil = filter(k, 1, data);
+
+% SPECTROGRAM
+figure('Position',[0,400,1500,350])
+spectrogram(data,window,window-step,2^nextpow2(window),fSamp,'yaxis')
+colormap(jet)
+at = gca;
+as = gca;
+at.XMinorTick = 'on';
+as.YMinorTick = 'on';
+
+% PLOT
+figure('Position',[0,100,1500,350]);
+plot(t,dfil,'b-')
+ax = gca;
+ay = gca;
+ax.XLabel.String = 'Time (Seconds)';
+ay.YLabel.String = 'Sampler Counts';
+ax.XLim = [0 size(t,2)/fSamp];
+ay.YLim = [-2300 2300];
+ax.XMinorTick = 'on';
+ay.YMinorTick = 'on';
+
